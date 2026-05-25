672f2f413919dc0ab250034a884088ec9f0c1b0d
klemp0 <caliniucnikita@gmail.com>
2026-05-25 13:03:26 +0300

    Ziua 1 - structura initiala

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  $titlu = "Biblioteca Mea";
+?>
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $titlu; ?></title>
+</head>
+<body>
+  <h1><?php echo $titlu; ?></h1>
+  <p>Serverul funcționează!</p>
+</body>
+</html>
