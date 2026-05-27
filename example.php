55f89cfc78b42c4c09b452ebf99d16dd7349108a
klemp0 <caliniucnikita@gmail.com>
2026-05-27 09:05:56 +0300

    ziua 3- am exersat is si for in php

--- example.php
+++ example.php
@@ -9,6 +9,30 @@
 <?php
     echo "Hello World!";
     echo "<script>console.log('Hello Console!');</script>";
+
+    $rezultat="";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input = $_POST["numere"];
+        $numere = explode(",", $input);
+
+        $pare=0;
+        $impare=0;
+
+        for ($i=0; $i < count($numere); $i++){
+            if($numere[$i] % 2 == 0){
+                $pare++;
+            } else {
+                $impare++;
+            }
+        }
+
+        $rezultat="Numere pare: " . $pare . " Numere impare: " . $impare;
+    }
 ?>
+<form method="POST">
+    <input type="text" name="numere" placeholder="...">
+    <button type="submit">Submit</button>
+</form>
+<p><?php echo $rezultat; ?></p>
 </body>
 </html>
