94760a500beb022fe16530a468c538ce9ad8aca4
klemp0 <caliniucnikita@gmail.com>
2026-06-01 20:34:30 +0300

    am terminat lucrul la formularul principal de inregistrare si l-am reparat close #5 close #6

--- php/auth.php
+++ php/auth.php
@@ -1,47 +1,58 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$repeat_password = $_POST["repeat_password"];
-
+session_start();
 $content = file_get_contents("../data/users.json");
 $users = json_decode($content, true);
 
-foreach($users as $user){
-    if($user["email"] == $email){
-         header("Location: /proiect_practica/index.php?form=register&error=email_exists");
+if (isset($_POST["register"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    foreach ($users as $user) {
+        if ($user["email"] == $email) {
+            $_SESSION["flash_error"] = "email_exists";
+            header("Location: /proiect_practica/index.php?form=register");
+            exit();
+        }
+    }
+
+    $newUser = ["name" => $name, "email" => $email, "password" => $password];
+    $users[] = $newUser;
+    $_SESSION["user_name"] = $name;
+    $_SESSION["user_email"] = $email;
+    file_put_contents("../data/users.json", json_encode($users, JSON_PRETTY_PRINT));
+    header("Location: /proiect_practica/page.php");
+    exit();
+}
+
+if (isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $emailFound = false;
+
+    foreach ($users as $user) {
+        if ($user["email"] == $email) {
+            $emailFound = true;
+            if ($user["password"] == $password) {
+                $_SESSION["user_name"] = $user["name"];
+                $_SESSION["user_email"] = $user["email"];
+                header("Location: /proiect_practica/page.php");
+                exit();
+            } else {
+                $_SESSION["flash_error"] = "login_error";
+                $_SESSION["flash_email"] = $email;
+                header("Location: /proiect_practica/index.php");
+                exit();
+            }
+        }
+    }
+
+    if (!$emailFound) {
+        $_SESSION["flash_error"] = "email_error";
+        $_SESSION["flash_email"] = $email;
+        header("Location: /proiect_practica/index.php");
         exit();
     }
 }
-
-$newUser = [
-    "name" => $name,
-    "email" => $email,
-    "password" => $password,
-    "repeat_password" => $repeat_password
-];
-
-$users[] = $newUser;
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    header("Location: /proiect_practica/index.php?form=register&error=invalid_email");
-    exit();
-}
-if(strlen($password) < 8){
-    header("Location: /proiect_practica/index.php?form=register&error=password_short");
-    exit();
-}
-if(!preg_match('/[a-z]/', $password) || !preg_match('/[A-Z]/', $password) || !preg_match('/[0-9]/', $password)){
-    header("Location: /proiect_practica/index.php?form=register&error=password_weak");
-    exit();
-}
-if($password != $repeat_password){
-    header("Location: /proiect_practica/index.php?form=register&error=password_mismatch");
-    exit();
-}
-
-file_put_contents("../data/users.json", json_encode($users, JSON_PRETTY_PRINT));
-
-header("Location: /proiect_practica/page.php");
-exit();
 ?>
